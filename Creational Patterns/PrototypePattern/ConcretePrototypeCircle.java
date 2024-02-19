e7d7f37ff92fb10a196481db7f057db288725a83
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-02-19 04:56:12 +0300

    Update ConcretePrototypeCircle.java

--- Creational Patterns/PrototypePattern/ConcretePrototypeCircle.java
+++ Creational Patterns/PrototypePattern/ConcretePrototypeCircle.java
@@ -1,1 +1,16 @@
 package PrototypePattern;
+
+public class ConcretePrototypeCircle implements Prototype{
+    private int radius;
+
+    public ConcretePrototypeCircle(int radius){
+        this.radius = radius ;
+    }
+    public ConcretePrototypeCircle(ConcretePrototypeCircle circle) {
+              this.radius = circle.radius;
+    }
+    @Override
+    public Prototype clone() {
+        return new ConcretePrototypeCircle(this);
+    }   
+}
