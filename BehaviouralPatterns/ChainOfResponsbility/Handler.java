893acbf00fb7d6e0f76ccfa14a0acae490487d29
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-02-25 04:49:41 +0300

    Create Handler.java

---
+++ BehaviouralPatterns/ChainOfResponsbility/Handler.java
@@ -0,0 +1,8 @@
+abstract class Handler{
+    protected Handler successor;
+
+    public void setSuccessor(Handler successor){
+        this.successor = successor;
+    }
+    public abstract void handleRequest(String request);
+}
