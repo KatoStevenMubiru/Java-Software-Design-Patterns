2002240a4f5acdad01a3646af765ef6617509a55
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-02-27 06:45:11 +0300

    Create Light.java

---
+++ BehaviouralPatterns/Command/Light.java
@@ -0,0 +1,10 @@
+package BEHAVIOURALPATTERNS.Command;
+
+class Light{
+    public void turnOff(){
+        System.out.println("The lights are on");
+    }
+    public void turnOn(){
+        System.out.println("The lights are off");
+    }
+}
