a50e5377e64e949aa21dd3d4c73c04f5c509f5f2
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-02-28 04:32:46 +0300

    Create LightOnCommand.java

---
+++ BehaviouralPatterns/Command/LightOnCommand.java
@@ -0,0 +1,13 @@
+package BEHAVIOURALPATTERNS.Command;
+
+class LightOnCommand implements Command{
+    private Light light;
+
+    public LightOnCommand(Light light){
+        this.light = light;
+    }
+    @Override
+    public void execute(){
+        light.turnOn();
+    }
+}
