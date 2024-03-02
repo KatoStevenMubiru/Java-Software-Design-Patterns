2ada836161adc1034b79ff4f6ed3db0e0392661e
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-03-02 05:27:49 +0300

    Create Circle.java

---
+++ StructuralPatterns/Decorator/Circle.java
@@ -0,0 +1,9 @@
+//Concrete class 1 implementing the interface
+
+public class Circle implements Shape{
+
+    @Override
+    public void draw(){
+        System.out.println(" Draw circle");
+    }
+}
