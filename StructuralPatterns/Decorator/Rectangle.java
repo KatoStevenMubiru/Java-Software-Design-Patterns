23c2e4bbb2db8964a16defe8fef0adc4f8c68288
Kato Steven Mubiru <kato.steven60@gmail.com>
2024-03-03 05:38:23 +0300

    Create Rectangle.java

---
+++ StructuralPatterns/Decorator/Rectangle.java
@@ -0,0 +1,8 @@
+//Concrete class 1 implementing the interface
+
+public class Rectangle implements Shape{
+    @Override 
+    public void draw(){
+        System.out.println("Draw rectangle");
+    }
+}
